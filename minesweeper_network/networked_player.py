bf99996e0acd92fd5370b5a3307c2dac7fee7a7f
sverrir <sverrir1993@gmail.com>
2016-12-14 21:37:20 +0000

    implemented the networked player and the observer board

---
+++ minesweeper_network/networked_player.py
@@ -0,0 +1,100 @@
+import sys
+import socket
+import select
+import threading
+
+class NetworkedPlayer:
+    """
+    The networked player class starts a thread
+    that accepts observing players and notifies
+    them with the state on a state change.
+    """
+
+    def __init__(self, host, port):
+        """
+        Constructs a new networked player and starts the
+        the server loop on a new thread.
+
+        :param host: The ip address of the host
+        :param port: The port number
+        """
+        self.state = ""
+        self.sockets = []
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((host,port))
+        self.server_socket.listen(10)
+        self.sockets.append(self.server_socket)
+        self.thread = threading.Thread(target=self.server_loop)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def server_loop(self):
+        """
+        Accepts new observers and notifies them with the current state.
+        If an observer leaves this method will also remove the observer
+        from the list of observers.
+
+        :return: None
+        """
+        while True:
+            ready_to_read, ready_to_write, in_error = select.select(self.sockets, [], [], 0)
+            for sock in ready_to_read:
+                if sock == self.server_socket:
+                    sock_fd, address = self.server_socket.accept()
+                    self.sockets.append(sock_fd)
+                    self.notify(sock_fd)
+                else:
+                    if sock in self.sockets:
+                        self.sockets.remove(sock)
+        sys.exit(0)
+
+    def notify_observers(self, state):
+        """
+        Notifies all the observers with the new state.
+
+        :param state: The new state
+        :return: None
+        """
+        self.state = state + "\r\n" if not state.endswith("\r\n") else state
+        for observer in self.sockets:
+            if observer != self.server_socket:
+                self.notify(observer)
+
+    def notify(self, sock):
+        """
+        Notifies a single observer with the state.
+
+        :param sock: The observer socket
+        :return: None
+        """
+        try:
+            sock.send(bytes(self.state, "ascii"))
+        except:
+            if sock in self.sockets:
+                self.sockets.remove(sock)
+
+if __name__ == "__main__":
+
+    def observer_function(state):
+        print("observer_function: {0}".format(state))
+
+    from minesweeper_network.observer_board import ObserverBoard
+
+    # Networked player is set up and in production is observing the player board
+    player = NetworkedPlayer("localhost", 80)
+
+    # When the player board updates it notifies player via its observer function
+    # This function in return notifies the remote observers
+    player.notify_observers("HHHH/HHHH/HHHH")
+
+    # A remote observer connects to the player
+    observer_board = ObserverBoard("localhost", 80)
+
+    # The ui observes the observer board
+    observer_board.add_observer(observer_function)
+
+    # State change happens at the player -> remote observers should be notified
+    # -> The ui at remote observer should be notified that is the observer_function
+    player.notify_observers("01HH/1HHH/HHHH")
+    player.notify_observers("01HH/1XHH/HBHH")
---
+++ minesweeper_network/observer_board.py
@@ -0,0 +1,59 @@
+import sys
+from minesweeper.board import Board
+import socket
+import threading
+
+class ObserverBoard(Board):
+    """
+    The observer board does not implement the
+    gameplay logic. It is networked and connects
+    to a networked player server. All board
+    updates will be notified from the player to
+    the observer board.
+    """
+
+    def __init__(self, player_ip, player_port):
+        """
+        Constructs the observer board.
+        Attempts to connect to the networked player
+        and receive its state. On successful connection
+        the Board will start a thread that listens to
+        the player activity.
+
+        :param player_ip: The ip address of the player.
+        :param player_port: The port number of the player.
+        :raises Exception: if connection or state retrieval fails.
+        """
+        try:
+            super(ObserverBoard, self).__init__()
+            self.player_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.player_socket.connect((player_ip, player_port))
+            self._receive_new_state()
+            self.thread = threading.Thread(target=self.client_loop)
+            self.thread.daemon = True
+            self.thread.start()
+        except:
+            print("Exception occurred please handle it.")
+
+    def client_loop(self):
+        """
+        Starts the client loop.
+        The loop will wait for activity from the player board
+        and notify the observers.
+        If the connection is lost to the player board the connection
+        loss will be handled.
+
+        :return:
+        """
+        while True:
+            self._receive_new_state()
+            self._notify_observers()
+        sys.exit(0)
+
+    def _receive_new_state(self):
+        """
+        waits for new state update from player
+        and processes the reponse.
+        :return: None
+        """
+        self._state = self.player_socket.recv(1024).decode("utf-8")
