d4b8bf5881a4bf31f677ee772b3cc64100b48ab7
kari14@ru.is <karieiriks>
2016-12-15 17:25:10 +0000

    UI

--- minesweeper_start.py
+++ minesweeper_start.py
@@ -55,14 +55,14 @@ class SelectLevelPage(tk.Frame):
 		level_label = tk.Label(self, text='Select a board size', 
 			font=('Helvetica',12))
 		level_label.pack(padx=10, pady=10)
-		small = tk.Button(self, text='8x16', bg='#CCCCCC', 
-			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(8,16,0.15)))
+		small = tk.Button(self, text='4x8', bg='#CCCCCC', 
+			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(4,8,0.15)))
 		small.pack()
-		medium = tk.Button(self, text='12x24', bg='#CCCCCC',
-			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(12,24,0.15)))
+		medium = tk.Button(self, text='8x16', bg='#CCCCCC',
+			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(8,16,0.15)))
 		medium.pack()
-		large = tk.Button(self, text='16x32', bg='#CCCCCC',
-			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(16,32,0.15)))
+		large = tk.Button(self, text='12x24', bg='#CCCCCC',
+			command=lambda: MinesweeperGameUI(self, self.create_minesweeper_board(12,24,0.15)))
 		large.pack()
 		back = tk.Button(self, text='Back', bg='#CCCCCC', 
 			command=lambda: controller.show_frame(StartingPage))
@@ -82,8 +82,10 @@ class MinesweeperGameUI(tk.Frame):
 	def __init__(self, master, board):
 		tk.Frame.__init__(self, master)
 		self.board = board
-		self.t = tk.Toplevel(self)
+		self.toplevel = tk.Toplevel(self)
 		self.buttons = {}
+		self.toolbar = None
+		self.grid = None
 		self.bomb_path = "images/bomb.jpg"
 		self.bomb_image = Image.open(self.bomb_path)
 		self.bomb_image = self.bomb_image.resize((35,38), Image.ANTIALIAS)
@@ -92,6 +94,14 @@ class MinesweeperGameUI(tk.Frame):
 		self.flag_image = Image.open(self.flag_path)
 		self.flag_image = self.flag_image.resize((35,38), Image.ANTIALIAS)
 		self.flag = ImageTk.PhotoImage(self.flag_image)
+		self.smile_cool_path = "images/smile.jpg"
+		self.smile_cool_image = Image.open(self.smile_cool_path)
+		self.smile_cool_image = self.smile_cool_image.resize((35,38), Image.ANTIALIAS)
+		self.smile_cool = ImageTk.PhotoImage(self.smile_cool_image)
+		self.smile_sad_path = "images/smile_lost.jpg"
+		self.smile_sad_image = Image.open(self.smile_sad_path)
+		self.smile_sad_image = self.smile_sad_image.resize((35,38), Image.ANTIALIAS)
+		self.smile_sad = ImageTk.PhotoImage(self.smile_sad_image)
 		self.colors = ["white", "blue", "green", "red", "#AA00FF", "#002Eb8", "magenta", "#FF6633", "black"]
 		board.add_observer(self.fill_board)
 
@@ -106,30 +116,44 @@ class MinesweeperGameUI(tk.Frame):
 			self.after(5, lambda: event.widget.config(relief=tk.SUNKEN))
 			
 
-	def hax(self, event):
-		event.widget.config(relief=tk.SUNKEN)
-
 	def fill_board(self, board_info):
-		terminal, number_of_flags, state = board_info.split(',')
-		print(terminal, number_of_flags)
+		terminal, self.number_of_flags, state = board_info.split(',')
+		print(terminal, self.number_of_flags)
 		state = state.split('/')
 		print(state)
-		if not self.buttons :
-			for x, row in enumerate(state) :
-				for y, col in enumerate(row) :
-					self.but = tk.Button(self.t, height=2, width=5 , bg='#CCCCCC')
-					self.but.grid(row=x, column=y)
-					self.but.row = x
-					self.but.col = y
-					self.buttons[(x,y)] = self.but
-					self.but.bind('<ButtonRelease-3>', self.right_click_event_handler)
-					self.but.bind('<ButtonRelease-1>', self.left_click_event_handler)
+		if not self.toolbar:
+			self.toolbar = tk.Frame(self.toplevel, bg='#EEEEEE')
+			self.smile_but = tk.Button(self.toolbar, image=self.smile_cool)
+			self.smile_but.bind('<ButtonRelease-1>', self.dostuff)
+			self.smile_but.grid(row=0, column=0)
+			self.flag_label = tk.Label(self.toolbar, text=self.number_of_flags, fg='blue')
+			self.flag_label.grid(row=0, column=1, padx=10)
+			self.toolbar.pack()
+		if not self.grid :
+			self.grid = tk.Frame(self.toplevel)
+			if not self.buttons :
+				for x, row in enumerate(state) :
+					for y, col in enumerate(row) :
+						self.but = tk.Button(self.grid, height=2, width=5 , bg='#CCCCCC')
+						self.but.grid(row=x, column=y)
+						self.but.row = x
+						self.but.col = y
+						self.buttons[(x,y)] = self.but
+						self.but.bind('<ButtonRelease-3>', self.right_click_event_handler)
+						self.but.bind('<ButtonRelease-1>', self.left_click_event_handler)
+			self.grid.pack()
 		self.update_button_states(board_info)
+
+	def dostuff(self, event):
+		print('Hommar og lesbiur')
 			
 
 	def update_button_states(self, board_info):
 		terminal, number_of_flags, state = board_info.split(',')
 		state = state.split('/')
+		self.flag_label = tk.Label(self.toolbar, text=self.number_of_flags, fg='blue')
+		self.flag_label.grid(row=0, column=1, padx=10)
+		self.toolbar.pack()
 		for x, row in enumerate(state) :
 			for y, col in enumerate(row) :	
 				self.but = self.buttons[(x,y)]
@@ -148,13 +172,23 @@ class MinesweeperGameUI(tk.Frame):
 							bg='#EEEEEE', relief=tk.SUNKEN, image='', height=2, width=5)
 				else :
 					self.but.config(image='', height=2, width=5)
-		if terminal == 'loss':
-			self.on_exit()
+		if terminal == 'loss' :
+			#self.on_defeat()
+			self.smile_but.config(image=self.smile_sad)
+			self.defeat = tk.Label(self.toolbar, text='You lost...', fg='red', font=('Helvetica', 16))
+			self.defeat.grid(row=0, column=2, padx=10)
+		if terminal == 'win' :
+			#self.on_win()
+			self.win = tk.Label(self.toolbar, text='You WON!', fg='green', font=('Helvetica', 16))
+			self.win.grid(row=0, column=2, padx=10)
 
 
-	def on_exit(self):
-		messagebox.showinfo("test", "test")
-		self.destroy() 
+	def on_win(self):
+		result = messagebox.askquestion("Minesweeper", "You won! Do you want to play again", parent=self.toplevel)
+
+	def on_defeat(self):
+		result = messagebox.askquestion("Minesweeper", "You lost.... Do you want to play again", parent=self.toplevel)
+
 
 
 class ObserverPage(tk.Frame):
@@ -165,7 +199,7 @@ class ObserverPage(tk.Frame):
 		players_label = tk.Label(self, text='Select a player to observe', font=('Helvetica', 12))
 		players_label.pack(padx=10, pady=10)
 		observe = tk.Button(self, text='Observe', bg='#CCCCCC', 
-			command=lambda: MinesweeperGameUI(self, ObserverBoard("localhost", 81)))
+			command=lambda: MinesweeperGameUI(self, ObserverBoard("localhost", 80)))
 		observe.pack()
 
 		back = tk.Button(self, text='Back', bg='#CCCCCC', 
@@ -178,10 +212,6 @@ class MinesweeperGame(tk.Frame):
 		self.height = size
 		self.width = size
 		print(size)
-
-def on_exit():
-	messagebox.showinfo("test", "test")
-	app.destroy() 
 
 app = Minesweeper()
 app.mainloop()
